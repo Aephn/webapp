af7ca20122e1fbc0179bf97c3b853cec214be925
Alivy <84561233+Aephn@users.noreply.github.com>
2024-06-21 01:23:51 -0600

    Add files via upload

---
+++ Main/input.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, url_for, render_template
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'   # define an upload folder
+
+# Ensure the upload folder exists
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+@app.route('/')
+def index():
+    return render_template('maptile.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print("Upload function called")
+    if 'file' not in request.files:
+        print("No file part")
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        print("No selected file")
+        return 'No selected file'
+    if file:
+        filename = file.filename
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        print(f"File saved: {filepath}")
+        return f'File uploaded successfully: {filename}'
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", debug=True)
---
+++ Main/static/scripts/mapManager.js
@@ -0,0 +1,72 @@
+// Handles Cursor Events
+L.CursorHandler = L.Handler.extend( {
+    addHooks: function () {
+        this._map.on('dblclick', this._addMarker, this);   // Create listener for double click at startup
+    },
+
+    removeHooks: function () {
+        this._map.off('dblclick', this._addMarker, this);  // Removes Listener for double click at close
+    },
+
+    _addMarker: function(e) {
+        createPin(e.latlng, "Marker")
+    }
+});
+
+L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);
+
+// map initialization
+var map = L.map('map', {
+    center: [0,0],
+    zoom: 0,
+    cursor: true,
+    doubleClickZoom: false,   // disable double click zoom
+    layers: [   // Jawg layer testing (ACCESS TOKEN INSIDE)
+        new L.tileLayer('https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+            attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: 0,
+            maxZoom: 22,
+            accessToken: 'Mk2XMa0mf65s97gaaKZ1Kta6vaoMsp3njJVG5uooQZT9SwflELWRhK5NLpKlA1AN'  // Be careful of this
+        })
+    ]
+}).setView([39.520845889671264, -104.9200172232749], 13);    // Defines the point where map opens to on startup
+
+// define location pin
+var location_icon = L.icon({
+    // Define pin icon and attributes
+    iconUrl: '../static/assets/loc_icon.png',   // path from html file to icon.
+    iconSize:     [38, 40], // size of the icon
+    iconAnchor:   [25, 35], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-5, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+function markerAnimation(marker) {
+    // Marker Bobbing Animations
+    marker.on("click", function() {
+        var pos = map.latLngToLayerPoint(marker.getLatLng());
+        pos.y -= 5;
+        var fx = new L.PosAnimation();
+  
+        fx.once('end',function() {
+            pos.y += 5;
+            fx.run(marker._icon, pos, 0.8);
+            });
+        fx.run(marker._icon, pos, 0.3);
+    });
+    return null;
+}
+
+function createPin(xy_pos=[0,0], message="") {
+    // Create Pin + Add Animation
+    var temp = new L.marker(xy_pos, {icon:location_icon, opacity:1.0, alt:message}).addTo(map).bindPopup(message);
+    markerAnimation(temp);
+    return null;
+}
+
+// Markers + Text
+createPin([39.520845889671264, -104.920017223274], "Rock Canyon High School");
+createPin([39.56336885892135, -104.87694732273937], "Park Meadows Mall");
+createPin([39.74099425098315, -104.9835222294324], "This is the Capital!");
+
+// Add map scale
+L.control.scale({maxWidth:100}).addTo(map);
