a9c8668627cb8dcb2bf8935de6f4c92b0bb702b0
Alivy <harrisontlin@gmail.com>
2024-06-23 23:34:58 -0600

    Consolidation of methods into a single Pin class

--- Main/static/scripts/mapManager.js
+++ Main/static/scripts/mapManager.js
@@ -9,10 +9,11 @@ L.CursorHandler = L.Handler.extend( {
     },
 
     _addMarker: function(e) {
-        createPin(e.latlng, "Marker", true);
+        new Pin(e.latlng, undefined, true);   // undefined skips the parameter.
     }
 });
 L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);
+
 
 // map initialization
 var map = L.map('map', {
@@ -33,73 +34,100 @@ var map = L.map('map', {
 }).setView([39.520845889671264, -104.9200172232749], 13);    // Defines where map opens to on startup
 
 
-// Marker Variables + Functions shit
-var location_icon = L.icon({
-    // Define pin icon and attributes
-    iconUrl: '../static/assets/loc_icon.png',   // path from html file to icon.
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 37] // point of the icon which will correspond to marker's location
-    // popupAnchor:  [-50, -30] // point from which the popup should open relative to the iconAnchor
-});
+class Pin {
+    constructor(xy_pos=[0,0], message="Unnamed Marker", popup_on_creation=false) {
+        try {
+            // console.log("Creating a new Pin at position:", xy_pos);
 
-function bobbingAnimation(marker) {
-    // Marker Bobbing Animations
-    var marker_displacement = 5;   // Specifies how much the marker is displaced
-
-    marker.on("click", function() {
-        var pos = map.latLngToLayerPoint(marker.getLatLng());
-        pos.y -= marker_displacement;
-        var fx = new L.PosAnimation();
-  
-        fx.once('end',function() {
-            pos.y += marker_displacement;
-            fx.run(marker._icon, pos, 0.8);
+            var location_icon = L.icon({
+                iconUrl: '../static/assets/loc_icon.png',   // path from HTML file to icon.
+                iconSize: [40, 40],
+                iconAnchor: [20, 37]
             });
-        fx.run(marker._icon, pos, 0.3);
-    });
-    return null;
+
+            // Create Pin + Add Animation
+            this.marker = new L.marker(xy_pos, {
+                icon: location_icon, 
+                opacity: 1.0, 
+                alt: message
+            })
+            .addTo(map)
+            .bindPopup(message);
+
+            console.log("Marker created:", this.marker);
+
+            // Bind methods to ensure correct `this` context
+            this.bobbingAnimation = this.bobbingAnimation.bind(this);
+            this.createPopupMenu = this.createPopupMenu.bind(this);
+
+            this.bobbingAnimation(this.marker);
+        
+            // Create popup on the marker if popup_on_creation = true
+            if (popup_on_creation) {
+                this.createPopupMenu(xy_pos);
+            }
+        } catch (error) {
+            console.error("Error in Pin constructor:", error);
+        }
+    }   // create pin and animate it + popup
+    
+    changePinMessage(pin, message="Unnamed Marker") {
+        // I need to figure out how to select specific pins that are created.
+        return;
+    }   // method to change the pin message (NOT DONE)
+        
+    createPopupMenu(xy_pos=[0,0]) {
+        try {
+            var popupContent = '<h2>Upload an Image</h2>' +
+            '<form action="/upload" method="post" enctype="multipart/form-data">'+
+            '<input type="file" name="file" accept="image/*">' +
+            '<input type="submit" value="Upload">' +
+            '</form>';
+                
+            var popup = L.popup({offset: L.point(0, -10)});
+            popup.setLatLng(xy_pos)
+                .addTo(map)
+                .setContent(popupContent);
+        } catch (error) {
+            console.error("Error in createPopupMenu:", error);
+        }
+        
+        return;
+    }   // method to create a new popup window for the marker.
+        
+    openPopupMenu(marker) {
+        // Need to add an event for on a marker click.
+        return;
+    }   // method to re-open the popup window for the marker.
+
+    bobbingAnimation(marker) {
+        try {
+            var marker_displacement = 5;   // Specifies how much the marker moves
+        
+            marker.on("click", () => {
+                var pos = map.latLngToLayerPoint(marker.getLatLng());
+                pos.y -= marker_displacement;
+                var fx = new L.PosAnimation();
+          
+                fx.once('end', () => {
+                    pos.y += marker_displacement;
+                    fx.run(marker._icon, pos, 0.8);
+                });
+                fx.run(marker._icon, pos, 0.3);
+            });
+        } catch (error) {
+            console.error("Error in bobbingAnimation:", error);
+        }
+    
+        return;
+    }   // method which defines the bobbing animation of the marker.
 }
 
-function createPin(xy_pos=[0,0], message="", open_popup=false) {
-    // Create Pin + Add Animation
-    var temp = new L.marker(xy_pos, {
-        icon:location_icon, 
-        opacity:1.0, 
-        alt:message})
-        .addTo(map)
-        .bindPopup(message);   // Need to remove this in favor of creating popup menu
-    
-    // Add animation
-    bobbingAnimation(temp);
-    
-    // Create popup on the marker if open_popup = true
-    if (open_popup) {
-        createPopupMenu(xy_pos);
-    }
 
-    return null;
-}
-
-function createPopupMenu(xy_pos=[0,0]) {
-    // defines html code for the popup (do I need to initalize this every time?)
-    // There has to be a better way to represent this.
-    var popupContent = '<h2>Upload an Image</h2>' +
-    '<form action="/upload" method="post" enctype="multipart/form-data">'+
-    '<input type="file" name="file" accept="image/*">' +
-    '<input type="submit" value="Upload">' +
-    '</form>';
-    
-    var popup = L.popup({offset: L.point(0, -10)});
-    popup.setLatLng(xy_pos)
-        .addTo(map)
-        .setContent(popupContent);
-}
-
-
-// Default Test Markers
-createPin([39.520845889671264, -104.920017223274], "Rock Canyon High School");
-createPin([39.56336885892135, -104.87694732273937], "Park Meadows Mall");
-createPin([39.74099425098315, -104.9835222294324], "This is the Capital!");
+// Default Test Pins
+new Pin([39.56336885892135, -104.87694732273937], "Park Meadows Mall", true);
+new Pin([39.520845889671264, -104.920017223274], "Rock Canyon High School", false);
+new Pin([39.74099425098315, -104.9835222294324], "This is the Capital!", false);
 
 
 // Add map scale in bottom left.
