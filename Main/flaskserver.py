e444e6b3ab50a1fb7e7645e106c766374e04d95f
Alivy <harrisontlin@gmail.com>
2024-09-10 16:12:35 -0600

    format rework

--- Main/flaskserver.py
+++ Main/flaskserver.py
@@ -3,15 +3,18 @@ from flask import Flask, request, redirect, url_for, render_template, abort, \
 from werkzeug.utils import secure_filename   # import for secure file name processing
 import os
 
+import loginmanager.py
+
 app = Flask(__name__)
 app.config['UPLOAD_PATH'] = os.path.join(os.path.dirname(__file__), '../uploads')
-app.config['MAX_CONTENT_LENGTH'] = 1000 * 1000   # max file upload size of 1 mb.
+app.config['MAX_CONTENT_LENGTH'] = 1000 * 1000 * 10  # max file upload size of 10 mb.
 Allowed_File_Extensions = {'jpg', 'png', 'jpeg'}
 
 if not os.path.exists(app.config['UPLOAD_PATH']):
     os.makedirs(app.config['UPLOAD_PATH'])
 
-# file initalizations
+# Template loaders
+#
 @app.route('/')
 def index():
     """loads map.html for flask"""
@@ -31,6 +34,9 @@ def login_page():
 def homepage():
     return render_template('homepage.html')
 
+
+# Helper Functions
+#
 def valid_file():
     # Abstract the file processing so upload_file is more readable.
     print("placeholder")
@@ -39,34 +45,33 @@ def rename_userfilename():
     # rename files to include a user id.
     print("placeholder")
 
-# process files and ensure uploads are secure.
+def allowed_file(filename):
+    """check if submitted file is valid."""
+    return '.' in filename and \
+        filename.split('.', 1)[1].lower() in Allowed_File_Extensions
+
+
+# Upload Functions
+#
 @app.route('/marker', methods=['POST'])
 def upload_file():
     """Uploads files to /uploads and processes errors and malicious filenames"""
     valid_upload_submitted = False
-    
-    DEBUG_filecount_iterable = 0
-    print("DEBUG: Upload function called")   # debug
 
     if 'image_file' not in request.files:
-        print("No valid files submitted! Reloading.")
+        print("No valid files submitted! (Reloading)")
         return redirect(url_for('marker'))
 
     for upload in request.files.getlist('image_file'):  # NOTE: Need to change the error message.
-        if upload.filename == '':
+        filename = secure_filename(upload.filename)   # cleans malicious filenames
+        if filename == '':
             print("Error: Blank File!")
             continue   # Skip blank files (check if this is bad!)
-            # return 'Error: Blank File!'
 
-        if upload and allowed_file(upload.filename):
-            filename = secure_filename(upload.filename)   # cleans possible malicious filenames
-            
-            if (filename):
+        if upload and allowed_file(filename):
+            if filename:   # NOTE: Redundant check?
                 filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
                 upload.save(filepath)
-
-                print(f"DEBUG: File #{DEBUG_filecount_iterable} saved: {filepath}")
-                DEBUG_filecount_iterable+=1
                 valid_upload_submitted = True
     
     # handle if all files submitted are invalid
@@ -76,21 +81,14 @@ def upload_file():
     else:
         abort(418)   # NOTE: I'm a teapot (NOT FOR RELEASE)
 
-def allowed_file(filename):
-    """check if submitted file is valid."""
-    return '.' in filename and \
-        filename.split('.', 1)[1].lower() in Allowed_File_Extensions
-
 @app.route('/uploads/<filename>')
-def upload(filename):
+def upload(filename): # need to rename for clarity.
     """Handles pushing image files to marker.html to display."""
     print(f"Attempting to fetch: {filename}")
     try:
         return send_from_directory(app.config['UPLOAD_PATH'], filename)  
     except:
         print(f"Failed to fetch: {filename}!")
-    
-# @login_required # flask decorator for a login requirement.
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Main/loginmanager.py
+++ Main/loginmanager.py
@@ -1,3 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template, abort, \
     send_from_directory
 import os
+
+# @login_required # flask decorator for a login requirement.
